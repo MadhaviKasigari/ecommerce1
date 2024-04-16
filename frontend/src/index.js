99b8222e9bbb4bc6cfe6d740dd540b4d5c8018a6
Madhavi <kashigarimadhavi456@gmail.com>
2024-04-16 21:24:11 +0530

    index.js commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-// import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
@@ -11,20 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      {/* <BrowserRouter> */}
       <App />
-      {/* </BrowserRouter> */}
     </React.StrictMode>
   </Provider>
 );
-
-// reportWebVitals();
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     {/* <BrowserRouter> */}
-//     <App />
-//     {/* </BrowserRouter> */}
-//   </Provider>,
-//   document.getElementById("root")
-// );
